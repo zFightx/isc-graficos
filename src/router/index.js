a210e5b16ceb9fb05466adbb4ae6d5741bd273ac
Alexandre Souza <alexandresc45@gmail.com>
2024-11-05 01:09:34 -0300

    feat(router): add navigation guard
    Checks if the user has uploaded a file and if false, returns to the home page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SelectDataView from '@/views/SelectDataView.vue'
 import GradeTableView from '@/views/GradeTableView.vue'
+import store from '@/store'
 
 const routes = [
     {
@@ -24,6 +25,15 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+    if((to.name === 'select' || to.name === 'grade') && store.state.fileTable.length == 0){
+        next({name: 'home'});
+    }
+    else{
+        next();
+    }
+});
 
 export default router
